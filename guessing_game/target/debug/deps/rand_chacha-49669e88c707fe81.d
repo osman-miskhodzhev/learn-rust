5a5104cd8269553e58de2cfdb24c680accdbd0a7
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-03 03:58:37 +0300

    сделал листин 2.3

---
+++ guessing_game/target/debug/deps/rand_chacha-49669e88c707fe81.d
@@ -0,0 +1,9 @@
+/home/osman/learn/learn-rust/guessing_game/target/debug/deps/rand_chacha-49669e88c707fe81.d: /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/osman/learn/learn-rust/guessing_game/target/debug/deps/librand_chacha-49669e88c707fe81.rlib: /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/osman/learn/learn-rust/guessing_game/target/debug/deps/librand_chacha-49669e88c707fe81.rmeta: /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs
+
+/home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/lib.rs:
+/home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/chacha.rs:
+/home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/rand_chacha-0.3.1/src/guts.rs:
---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/guessing_game/target/debug/guessing_game: /home/osman/learn/learn-rust/guessing_game/src/main.rs
