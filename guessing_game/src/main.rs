3c67ddd9c3b9751c85c8e93ee3be92004e8047f9
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-04 23:28:25 +0300

    +1

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,8 +1,11 @@
+use std::cmp::Ordering;
 use std::io;
 
 use rand::Rng;
 
 fn main() {
+    // --snip--
+    
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
@@ -17,6 +20,14 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
 
