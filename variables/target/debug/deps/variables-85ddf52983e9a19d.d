d8919a6584463c7fb0eb5060ddef158eba6be56c
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-05 22:13:13 +0300

    3.1 end

---
+++ variables/target/debug/deps/variables-85ddf52983e9a19d.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/variables/target/debug/deps/variables-85ddf52983e9a19d.d: src/main.rs
+
+/home/osman/learn/learn-rust/variables/target/debug/deps/variables-85ddf52983e9a19d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/variables/target/debug/variables: /home/osman/learn/learn-rust/variables/src/main.rs
