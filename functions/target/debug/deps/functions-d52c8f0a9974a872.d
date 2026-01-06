1e9b503a549f9d98048a5bb65da032b5fae04a70
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-07 02:33:42 +0300

    +1 пример для 3.3

---
+++ functions/target/debug/deps/functions-d52c8f0a9974a872.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/functions/target/debug/deps/functions-d52c8f0a9974a872.d: src/main.rs
+
+/home/osman/learn/learn-rust/functions/target/debug/deps/functions-d52c8f0a9974a872: src/main.rs
+
+src/main.rs:
