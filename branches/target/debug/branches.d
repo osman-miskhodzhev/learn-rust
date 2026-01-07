bb5f3ac4075a7a0e36894b65197062fc9424b1e3
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-08 00:25:36 +0300

    пример из ветвлений

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/branches/target/debug/branches: /home/osman/learn/learn-rust/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-885af15583c6d3eb.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/branches/target/debug/deps/branches-885af15583c6d3eb.d: src/main.rs
+
+/home/osman/learn/learn-rust/branches/target/debug/deps/branches-885af15583c6d3eb: src/main.rs
+
+src/main.rs:
