1fe214c21b227e3bcd2c03dae6bca679e58fbeaa
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-12 03:41:22 +0300

    init slice для 4.3

---
+++ slices/target/debug/deps/slices-818ea23bfda24f8a.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/slices/target/debug/deps/slices-818ea23bfda24f8a.d: src/main.rs
+
+/home/osman/learn/learn-rust/slices/target/debug/deps/slices-818ea23bfda24f8a: src/main.rs
+
+src/main.rs:
---
+++ slices/target/debug/slices.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/slices/target/debug/slices: /home/osman/learn/learn-rust/slices/src/main.rs
