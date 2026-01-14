d9c1a207a4445c15ab83b3518b08215513b8e2f8
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-14 22:33:30 +0300

    +1  для 4.3

--- slices/src/main.rs
+++ slices/src/main.rs
@@ -1,5 +1,3 @@
-fn main() {}
-
 fn first_word(s: &String) -> usize {
     let bytes = s.as_bytes();
 
@@ -10,5 +8,16 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    // word still has the value 5 here, but s no longer has any content that we
+    // could meaningfully use with the value 5, so word is now totally invalid!
 }
 
