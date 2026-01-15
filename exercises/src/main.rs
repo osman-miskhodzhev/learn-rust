8cf318b8cdca2778e642e14470f44933b994ebe2
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-16 01:27:28 +0300

    +1 упражнение на функции

--- exercises/src/main.rs
+++ exercises/src/main.rs
@@ -6,9 +6,15 @@ fn rectangle_area(width: f64, heigth: f64) -> f64 {
     width * heigth
 }
 
+fn is_even(n: i32) -> bool {
+    n % 2 == 0
+}
+
 fn main() {
     println!("ex 1: ");
     println!("{}", greet("Osman"));
     println!("ex 2: ");
     println!("{}", rectangle_area(5.0, 3.0)); 
+    println!("ex 3: ");
+    println!("{}", is_even(4));
 }
