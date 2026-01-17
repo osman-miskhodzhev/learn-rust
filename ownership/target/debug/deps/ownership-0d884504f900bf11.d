f3d3e62a5ee42fe2951f1e07d51c60a613fb1d42
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-17 20:36:46 +0300

    добавил рисовалку для задачек

---
+++ ownership/target/debug/deps/ownership-0d884504f900bf11.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/ownership/target/debug/deps/ownership-0d884504f900bf11.d: src/main.rs
+
+/home/osman/learn/learn-rust/ownership/target/debug/deps/ownership-0d884504f900bf11: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/ownership.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/ownership/target/debug/ownership: /home/osman/learn/learn-rust/ownership/src/main.rs
