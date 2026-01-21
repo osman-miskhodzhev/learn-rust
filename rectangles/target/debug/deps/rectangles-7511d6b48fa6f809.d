d9cb569815820d802abd32f0f3a537bed999bda2
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-22 00:41:57 +0300

    +1 пример для 5.2

---
+++ rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d
@@ -0,0 +1,5 @@
+/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d: src/main.rs
+
+/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809: src/main.rs
+
+src/main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+/home/osman/learn/learn-rust/rectangles/target/debug/rectangles: /home/osman/learn/learn-rust/rectangles/src/main.rs
