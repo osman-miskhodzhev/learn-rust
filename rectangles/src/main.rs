c4cde2181dc0b0767e72fcafcc1fcc4c5362847a
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-01-23 10:17:37 +0300

    +1 пример для 5.3

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,12 +4,27 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
 
-    println!("rect1 is {rect1:?}");
+    if rect1.width() {
+        println!("так и живем");
+    }
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
 }
 
