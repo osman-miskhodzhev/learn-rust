22c90c42d7e0813bd39e094cdd3f0ee2308e3a3c
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-08 20:20:55 +0300

    + еще enum код

--- enums_ex/src/main.rs
+++ enums_ex/src/main.rs
@@ -5,15 +5,18 @@ fn main() {
         Five,
         Ten,
     }
-    fn hello_coin(coin: Coin) -> u8 {
+    fn hello_coin(coin: Coin) {
         match coin {
-            Coin::One => 1,
-            Coin::Two => 2,
-            Coin::Five => 5,
-            Coin::Ten => 10,
+            Coin::One => {println!("1 рубль");},
+            Coin::Two => {println!("2 рубля");},
+            Coin::Five => {println!("5 рублей");},
+            Coin::Ten => {println!("10 рублей");},
         }
     }
     
-    println!("{}", hello_coin(Coin::One));
+    hello_coin(Coin::One);
+    hello_coin(Coin::Two);
+    hello_coin(Coin::Five);
+    hello_coin(Coin::Ten);
 }
 
