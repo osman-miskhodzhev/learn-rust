749c8ea57f783062e2f03978a2bfeb718d74be92
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-09 19:08:47 +0300

    + пример с заданными типами и вывыводом

--- enums_ex/src/main.rs
+++ enums_ex/src/main.rs
@@ -2,7 +2,17 @@ enum Message {
     Text(String),
     Number(i32),
 }
+fn process_msg(message: Message) {
+    match message {
+        Message::Text(s) => {println!("Текстовое сообщение. Содержание: {}", s);},
+        Message::Number(n) => {println!("Номер. Содержание: {}", n);},
+    }
+}
+
 fn main() {
     let msg1 = Message::Text(String::from("Hello!"));
     let msg2 = Message::Number(50);
+    
+    process_msg(msg1);
+    process_msg(msg2);
 }
