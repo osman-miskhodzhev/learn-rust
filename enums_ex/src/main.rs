a2fb3169817dcedb6d91d993541dec7dc211c455
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-09 19:00:58 +0300

    + пример с заданными типами

--- enums_ex/src/main.rs
+++ enums_ex/src/main.rs
@@ -1,38 +1,8 @@
-fn main() {
-    enum Coin {
-        One,
-        Two,
-        Five,
-        Ten,
-    }
-    fn hello_coin(coin: Coin) {
-        match coin {
-            Coin::One => {println!("1 рубль");},
-            Coin::Two => {println!("2 рубля");},
-            Coin::Five => {println!("5 рублей");},
-            Coin::Ten => {println!("10 рублей");},
-        }
-    }
-    
-    hello_coin(Coin::One);
-    hello_coin(Coin::Two);
-    hello_coin(Coin::Five);
-    hello_coin(Coin::Ten);
-    
-    enum TheBill {
-        Five,
-        Ten,
-        Fifty,
-    }
-    fn hello_bill(bill: TheBill) {
-        match bill {
-            TheBill::Five => {println!("Малютка 5-ти рублевая");},
-            TheBill::Ten => {println!("Классика");},
-            TheBill::Fifty => {println!("50 рублей");},
-        }
-    }
-    hello_bill(TheBill::Five);
-    hello_bill(TheBill::Ten);
-    hello_bill(TheBill::Fifty);
+enum Message {
+    Text(String),
+    Number(i32),
 }
-
+fn main() {
+    let msg1 = Message::Text(String::from("Hello!"));
+    let msg2 = Message::Number(50);
+}
