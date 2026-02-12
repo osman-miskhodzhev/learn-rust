b12d8c06ee0347bee6d87a5febe3f38271466bbc
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-12 21:42:42 +0300

    добавление функции в область видимости с use неидиоматическим способом

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -4,9 +4,9 @@ mod front_of_house {
     }
 }
 
-use crate::front_of_house::hosting;
+use crate::front_of_house::hosting::add_to_waitlist;
 
 pub fn eat_at_restaurant() {
-    hosting::add_to_waitlist();
+    add_to_waitlist();
 }
 
