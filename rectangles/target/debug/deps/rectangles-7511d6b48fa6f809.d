048423aef6103f345d2418c428e4e5b09405c12b
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-15 04:29:55 +0300

    init new project

--- rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d
+++ rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d
@@ -1,5 +1,6 @@
-/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d: src/main.rs
+/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809.d: src/main.rs src/shape.rs
 
-/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809: src/main.rs
+/home/osman/learn/learn-rust/rectangles/target/debug/deps/rectangles-7511d6b48fa6f809: src/main.rs src/shape.rs
 
 src/main.rs:
+src/shape.rs:
