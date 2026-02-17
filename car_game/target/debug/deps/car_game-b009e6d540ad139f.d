1687eef35705b480fa44e7159b17266ff101d6f6
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-17 16:53:41 +0300

    обновлен гитигнор

--- car_game/target/debug/deps/car_game-b009e6d540ad139f.d
+++
@@ -1,5 +0,0 @@
-/home/osman/learn/learn-rust/car_game/target/debug/deps/car_game-b009e6d540ad139f.d: src/main.rs
-
-/home/osman/learn/learn-rust/car_game/target/debug/deps/libcar_game-b009e6d540ad139f.rmeta: src/main.rs
-
-src/main.rs:
--- car_game/target/debug/deps/color_quant-a7170cf0bea17c8e.d
+++
@@ -1,6 +0,0 @@
-/home/osman/learn/learn-rust/car_game/target/debug/deps/color_quant-a7170cf0bea17c8e.d: /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs
-
-/home/osman/learn/learn-rust/car_game/target/debug/deps/libcolor_quant-a7170cf0bea17c8e.rmeta: /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs /home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs
-
-/home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/lib.rs:
-/home/osman/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/color_quant-1.1.0/src/math.rs:
