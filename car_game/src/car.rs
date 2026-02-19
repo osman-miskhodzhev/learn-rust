d6c19ec7fe138f53c4a040030c8850cab9fd56d1
miskhodzh <miskhodzhev.osman@yandex.ru>
2026-02-19 11:47:14 +0300

    разделение по файлам

--- car_game/src/car.rs
+++ car_game/src/car.rs
@@ -1,27 +1,62 @@
-use std::f64::consts::PI;
+use macroquad::prelude::*;
 
-struct Car {
+pub struct Car {
     pos: Vec2,
+    radius: f32,
+    base_speed: f32,
+    boost: f32,
     angle: f32,
-    vel: f32,
-    steer: f32,
-
-    l: f32,
-    max_steer: f32,
-    max_speed: f32,
+    rotation_speed: f32,
+    texture: Texture2D,
 }
 
 impl Car {
-    pub fn new(x: f32, y:f32) -> Self {
-        Self {
-            pos: Vec2::new(x, y),
+    pub async fn new(x: f32, y: f32) -> Self {
+        let texture = load_texture("car.png").await.unwrap();
+        
+        Self { 
+            pos: Vec2::new(x, y), 
+            radius: 2.0, 
+            base_speed: 2.0, 
+            boost: 1.0,
             angle: 0.0,
-            vel: 0.0,
-            steer: 0.0,
-
-            l: 2.5,
-            max_steer: 30.0_f32.to_radians(),
-            max_speed: 10.0,
+            rotation_speed: 0.5,
+            texture,
         }
+    }
+
+    pub fn update(&mut self) {
+        let current_speed = if is_key_down(KeyCode::LeftShift) {
+            self.base_speed + self.boost
+        } else {
+            self.base_speed
+        };
+
+        if is_key_down(KeyCode::Right) { self.pos.x += current_speed; }
+        if is_key_down(KeyCode::Left) { self.pos.x -= current_speed; }
+        if is_key_down(KeyCode::Up) { self.pos.y -= current_speed; }
+        if is_key_down(KeyCode::Down) { self.pos.y += current_speed; }
+
+        self.pos.x = self.pos.x.clamp(self.radius, screen_width() - self.radius);
+        self.pos.y = self.pos.y.clamp(self.radius, screen_height() - self.radius);
+    }
+
+    pub fn draw(&self) {
+        let scale_x = 0.1;
+        let scale_y = 0.1;
+        
+        draw_texture_ex(
+            &self.texture,
+            self.pos.x - (self.texture.width() * scale_x) / 2.0,
+            self.pos.y - (self.texture.height() * scale_y) / 2.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(Vec2::new(
+                    self.texture.width() * scale_x,
+                    self.texture.height() * scale_y
+                )),
+                ..Default::default()
+            }
+        );
     }
 }
--- car_game/src/main.rs
+++ car_game/src/main.rs
@@ -1,69 +1,13 @@
 use macroquad::prelude::*;
-
-struct Car {
-    pos: Vec2,
-    radius: f32,
-    base_speed: f32,
-    boost: f32,
-    texture: Texture2D,  // Добавляем поле для текстуры
-}
-
-impl Car {
-    async fn new(x: f32, y: f32) -> Self {  // Теперь функция асинхронная
-        // Загружаем текстуру (путь к файлу изображения)
-        let texture = load_texture("car.png").await.unwrap();
-        
-        Self { 
-            pos: Vec2::new(x, y), 
-            radius: 2.0, 
-            base_speed: 2.0, 
-            boost: 1.0,
-            texture,  // Сохраняем текстуру
-        }
-    }
-
-    fn update(&mut self) {
-        let current_speed = if is_key_down(KeyCode::LeftShift) {
-            self.base_speed + self.boost
-        } else {
-            self.base_speed
-        };
-
-        if is_key_down(KeyCode::Right) { self.pos.x += current_speed; }
-        if is_key_down(KeyCode::Left) { self.pos.x -= current_speed; }
-        if is_key_down(KeyCode::Up) { self.pos.y -= current_speed; }
-        if is_key_down(KeyCode::Down) { self.pos.y += current_speed; }
-
-        self.pos.x = self.pos.x.clamp(self.radius, screen_width() - self.radius);
-        self.pos.y = self.pos.y.clamp(self.radius, screen_height() - self.radius);
-    }
-
-    fn draw(&self) {
-       let scale_x = 0.1;  // Масштаб по X
-    let scale_y = 0.1;  // Масштаб по Y
-    
-    draw_texture_ex(
-        &self.texture,
-        self.pos.x - (self.texture.width() * scale_x) / 2.0,
-        self.pos.y - (self.texture.height() * scale_y) / 2.0,
-        WHITE,
-        DrawTextureParams {
-            dest_size: Some(Vec2::new(
-                self.texture.width() * scale_x,
-                self.texture.height() * scale_y
-            )),
-            ..Default::default()
-        }
-    );
-    }
-}
+mod car;
+use car::Car;
 
 #[macroquad::main("Simple")]
 async fn main() {
-    let mut car = Car::new(100.0, 350.0).await;  // Ждем загрузки текстуры
+    let mut car = Car::new(100.0, 350.0).await;
     
     loop {
-        clear_background(BLACK);
+        clear_background(GRAY);
         car.update();
         car.draw();
         next_frame().await;
